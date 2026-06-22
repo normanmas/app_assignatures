c8140315a04bd1411da99fac55051a8708ceeec5
Norman Masià <norman71@gmail.com>
2026-06-22 19:08:04 +0200

    Afegir la pàgina de les assignatures

--- app.py
+++ app.py
@@ -4,30 +4,32 @@ from flask import Flask, render_template
 # Crear l'aplicació web
 aplicacio = Flask(__name__)
 
+# Valors de prova per a les assignatures
+assignatures = [
+    {
+        "nom": "Matemàtiques",
+        "credits": 6,
+        "semestre": "Primer semestre"
+    },
+    {
+        "nom": "Programació 1",
+        "credits": 6,
+        "semestre": "Primer semestre"
+    },
+    {
+        "nom": "Bases de dades",
+        "credits": 6,
+        "semestre": "Segon semestre"
+    }
+]
 
 @aplicacio.route("/")
 def inici():
-    assignatures = [
-        {
-            "nom": "Matemàtiques",
-            "credits": 6,
-            "semestre": "Primer semestre"
-        },
-        {
-            "nom": "Programació 1",
-            "credits": 6,
-            "semestre": "Primer semestre"
-        },
-        {
-            "nom": "Bases de dades",
-            "credits": 6,
-            "semestre": "Segon semestre"
-        }
+    return render_template("index.html")
 
-    ]
-
-    #
-    return render_template("index.html", assignatures=assignatures)
+@aplicacio.route("/assignatures")
+def veure_assignatures():
+    return render_template("assignatures.html", assignatures = assignatures)
 
 if __name__ == "__main__":
     aplicacio.run(debug=True)
